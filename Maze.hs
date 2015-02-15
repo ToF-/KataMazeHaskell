23e25ce41ead50d3450701df64f3d55c398331f1
ToF- <cthibaut@octo.com>
2015-02-15 23:50:38 +0100

    capacity to scan mazes

--- Maze.hs
+++ Maze.hs
@@ -8,5 +8,26 @@ data Maze = Maze { a :: Coords,
 
 type Coords = (Integer,Integer)
 
-scanMaze :: [String] -> Maze
-scanMaze _ = Maze (0,0) (0,1) [(0,0),(0,1)]
+defaultMaze :: Maze
+defaultMaze = Maze (-1,-1) (-1,-1) []
+
+scanMaze :: String -> Maze
+scanMaze = normalize . fst . foldl scanLine (defaultMaze,(0,0)) 
+
+normalize :: Maze -> Maze
+normalize m = m { rooms = reverse (rooms m)}
+
+scanLine :: (Maze,Coords) -> Char -> (Maze,Coords)
+scanLine (m,(r,c))  '#' = (m, (r, c+1))
+scanLine (m,(r,c)) '\n' = (m, (r+1, 0))
+scanLine (m,(r,c))  ch  = (updateMaze m (r, c) ch, (r, c+1))
+
+updateMaze :: Maze -> Coords -> Char -> Maze
+updateMaze m cd '.' = addRoom cd m 
+updateMaze m cd 'A' = addRoom cd (m { a = cd })
+updateMaze m cd 'B' = addRoom cd (m { b = cd })
+
+addRoom :: Coords -> Maze -> Maze
+addRoom cd m@(Maze _ _ rs) = m { rooms = cd:rs } 
+
+
--- Specs.hs
+++ Specs.hs
@@ -6,8 +6,38 @@ import Maze
 main = hspec $ do
     describe "scanMaze" $ do
         it "should read a very simple maze" $ do
-            scanMaze ["AB"] `shouldBe` 
+            scanMaze "AB" `shouldBe` 
                 Maze { a = (0,0),
                        b = (0,1),
                        rooms = [(0,0),(0,1)] }
 
+        it "should find A and B locations" $ do
+            scanMaze "BA" `shouldBe` 
+                Maze { a = (0,1),
+                       b = (0,0),
+                       rooms = [(0,0),(0,1)] }
+
+        it "should read lines longer than 2" $ do
+            scanMaze "A.B" `shouldBe` 
+                Maze { a = (0,0),
+                       b = (0,2),
+                       rooms = [(0,0),(0,1),(0,2)] }
+
+        it "should read more than 1 line" $ do
+            scanMaze "A\nB" `shouldBe` 
+                Maze { a = (0,0),
+                       b = (1,0),
+                       rooms = [(0,0),(1,0)] }
+
+        it "should ignore walls" $ do
+            scanMaze "A#B" `shouldBe` 
+                Maze { a = (0,0),
+                       b = (0,2),
+                       rooms = [(0,0),(0,2)] }
+
+        it "should process e.g. \n##A##\n#...#\n#.#.#\n###B#\ncorrectly" $ do
+            let t="##A##\n#...#\n#.#.#\n###B#"
+            scanMaze t `shouldBe` 
+                Maze {a = (0,2), 
+                      b = (3,3), 
+                      rooms = [(0,2),(1,1),(1,2),(1,3),(2,1),(2,3),(3,3)]}
