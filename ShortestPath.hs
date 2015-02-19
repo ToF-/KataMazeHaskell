9516d397e98bb1e4e5822312caa6df37eae9698f
ToF- <cthibaut@octo.com>
2015-02-19 22:21:56 +0100

    starting shortest paths

--- ShortestPath.hs
+++ ShortestPath.hs
@@ -1,7 +1,15 @@
 module ShortestPath
 where
+import Data.Map (Map)
+import qualified Data.Map as M
 
-data Node a = Node a
-type Edge = (Node a, Node a, b)
+type Node  = Integer
+type Edge  = (Node, Node, Label)
+type Label = Integer
+type Graph = Map Node [(Node,Label)]
+type Step  = (Node, Label, Node)
 
-fromList :: [(Node a, [Edge])]
+shortestPaths :: Graph -> Node -> [Step]
+shortestPaths g s = [(s,0,s),(d,l,s)]
+    where
+    (d,l) = head $ M.findWithDefault [] s g
--- SpecShortestPath.hs
+++ SpecShortestPath.hs
@@ -2,9 +2,14 @@ module SpecShortestPath
 where
 import Test.Hspec
 import ShortestPath
+import Data.Map (Map)
+import qualified Data.Map as M
 
 main = hspec $ do
-    describe "shortest path function" $ do
-        it "should find source itself for trivial graph of one node (no path at all) " $ do
-            let g = fromList [('A',[])]
-            shortestPath g 'A' `shouldBe` (O, []) 
+    describe "shortest paths function" $ do
+        it "should find simple paths from graphs with two nodes" $ do 
+            let g = M.fromList [(1,[(2,100)]),(2,[(1,100)])]
+            shortestPaths g 1  `shouldBe` [(1,0,1),(2,100,1)] 
+
+            let g = M.fromList [(5,[(7,130)]),(7,[(5,130)])]
+            shortestPaths g 5  `shouldBe` [(5,0,5),(7,130,5)] 
