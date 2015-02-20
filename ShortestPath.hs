00b4b01f22f2cf7efa281b5a8f4979da6375ff8d
ToF- <cthibaut@octo.com>
2015-02-20 22:30:58 +0100

    decomposing

--- ShortestPath.hs
+++ ShortestPath.hs
@@ -2,14 +2,22 @@ module ShortestPath
 where
 import Data.Map (Map)
 import qualified Data.Map as M
+import Data.PSQueue (PSQ)
+import Data.PSQueue as Q (empty, insert)
 
-type Node  = Integer
-type Edge  = (Node, Node, Label)
-type Label = Integer
-type Graph = Map Node [(Node,Label)]
-type Step  = (Node, Label, Node)
 
-shortestPaths :: Graph -> Node -> [Step]
-shortestPaths g s = [(s,0,s),(d,l,s)]
-    where
-    (d,l) = head $ M.findWithDefault [] s g
+type Node  = Int
+type Label = Int
+type Distance = Int
+type Edge  = (Node, Label)
+type Graph = Map Node [Edge]
+type Path  = Map Node (Distance, Node)
+type Step  = (Node, (Distance, Node))
+type Queue = PSQ Node (Distance, Node)
+
+infinity :: Int
+infinity = maxBound
+
+
+initialize :: Graph -> Node ->  Queue
+initialize g _ = Q.empty 
--- SpecShortestPath.hs
+++ SpecShortestPath.hs
@@ -4,12 +4,12 @@ import Test.Hspec
 import ShortestPath
 import Data.Map (Map)
 import qualified Data.Map as M
+import Data.PSQueue (PSQ)
+import Data.PSQueue as Q (empty, insert, fromList, toList, Binding (..))
 
 main = hspec $ do
-    describe "shortest paths function" $ do
-        it "should find simple paths from graphs with two nodes" $ do 
-            let g = M.fromList [(1,[(2,100)]),(2,[(1,100)])]
-            shortestPaths g 1  `shouldBe` [(1,0,1),(2,100,1)] 
-
-            let g = M.fromList [(5,[(7,130)]),(7,[(5,130)])]
-            shortestPaths g 5  `shouldBe` [(5,0,5),(7,130,5)] 
+    describe "initialize" $ do
+        it "fills the priority queue with infinity distances except for source" $ do 
+            let g = M.fromList [(1,[(2,100)]),(1,[(3,150)]),(2,[(3,20)]),(3,[])]
+            let q = Q.fromList [1 :-> (infinity,1), 2 :-> (0,2), 3 :-> (infinity,3)]
+            toList (initialize g 2) `shouldBe` (toList q)
