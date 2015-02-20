50a8258256bd2c950aef663dd3bd633d5bcc5db9
ToF- <cthibaut@octo.com>
2015-02-21 00:28:28 +0100

    corrected test

--- SpecShortestPath.hs
+++ SpecShortestPath.hs
@@ -10,6 +10,6 @@ import Data.PSQueue as Q (empty, insert, fromList, toList, Binding (..))
 main = hspec $ do
     describe "initialize" $ do
         it "fills the priority queue with infinity distances except for source" $ do 
-            let g = M.fromList [(1,[(2,100)]),(1,[(3,150)]),(2,[(3,20)]),(3,[])]
+            let g = M.fromList [(1,[(2,100),(3,150)]),(2,[(1,100),(3,20)]),(3,[(1,150),(2,20)])]
             let q = Q.fromList [1 :-> (infinity,1), 2 :-> (0,2), 3 :-> (infinity,3)]
             toList (initialize g 2) `shouldBe` (toList q)
