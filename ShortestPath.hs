87f58974d9597c47600c5c5ad1ba0bebbc024238
ToF- <cthibaut@octo.com>
2015-02-21 22:44:31 +0100

    shortestPath failing (to be done)

--- ShortestPath.hs
+++ ShortestPath.hs
@@ -1,9 +1,9 @@
 module ShortestPath
 where
 import Data.Map (Map)
-import qualified Data.Map as M (insert, keys)
+import qualified Data.Map as M (insert, keys, lookup)
 import Data.PSQueue (PSQ)
-import Data.PSQueue as Q (empty, insert, deleteMin, minView, Binding (..))
+import Data.PSQueue as Q (empty, insert, deleteMin, minView, Binding (..), lookup, adjust)
 
 
 type Node  = Int
@@ -23,11 +23,35 @@ initialize g n = foldl insertNode Q.empty (M.keys g)
     where
     insertNode :: Queue -> Node -> Queue 
     insertNode q m = Q.insert m (distance,m) q
-        where distance = if m == n then 0 else infinity
+        where
+        distance | m == n    = 0
+                 | otherwise = infinity
 
 settle :: (Queue,Path) -> (Queue,Path)
 settle (q,p) = case Q.minView q of
-        Nothing             -> error "unexpected condition: empty PSQueue" 
-        Just (m :-> d, q')  -> (q', M.insert m d p)
+    Just (m :-> d, q')  -> (q', M.insert m d p)
+    Nothing             -> error "unexpected condition: empty PSQueue" 
     
-    
+neighbors :: Node -> Distance -> Graph -> [(Node,Distance)]
+neighbors n d g = case M.lookup n g of 
+    (Just es) -> map addDistance es
+        where
+        addDistance (m,e) = (m,e+d)    
+
+    Nothing -> error "unexpected condition: node not found"
+
+adjust :: Node -> [(Node,Distance)] -> Queue -> Queue
+adjust n ds q = foldl (adjustNode n) q ds
+    where
+    adjustNode :: Node -> Queue -> (Node,Distance) -> Queue
+    adjustNode n q (m,d) | (currentDistance m q) > d = Q.adjust (const (d,n)) m q
+        where 
+        currentDistance :: Node -> Queue -> Distance
+        currentDistance m q = case Q.lookup m q of
+            (Just (d',_)) -> d'
+            Nothing -> error "unexpected condition: node not in queue"
+    adjustNode n q _  = q
+
+shortestPath :: Graph -> Queue -> Path -> (Queue, Path)
+shortestPath g q p = settle (q,p)
+
--- SpecShortestPath.hs
+++ SpecShortestPath.hs
@@ -3,23 +3,53 @@ where
 import Test.Hspec
 import ShortestPath
 import Data.Map (Map)
-import qualified Data.Map as M
+import qualified Data.Map as M (empty, toList, fromList)
 import Data.PSQueue (PSQ)
-import Data.PSQueue as Q (empty, insert, fromList, toList, Binding (..), lookup)
+import Data.PSQueue as Q (empty, insert, fromList, toList, Binding (..), lookup, size)
 
 main = hspec $ do
     describe "initialize" $ do
         it "fills the priority queue with infinity distances except for source" $ do 
             let g = M.fromList [(1,[(2,100),(3,150)]),(2,[(1,100),(3,20)]),(3,[(1,150),(2,20)])]
             let q = Q.fromList [1 :-> (infinity,1), 2 :-> (0,2), 3 :-> (infinity,3)]
+
             toList (initialize g 2) `shouldBe` (toList q)
 
     describe "settle" $ do
         it "remove the least distant node from the queue and insert it in the path" $ do
             let g = M.fromList [(1,[(2,100),(3,150)]),(2,[(1,100),(3,20)]),(3,[(1,150),(2,20)])]
             let q = initialize g 2
-            let (q',m) = settle (q,M.empty)
+
+            let (q',p) = settle (q,M.empty)
             Q.lookup 2 q' `shouldBe` Nothing
-            M.lookup 2 m  `shouldBe` Just (0,2)
+            Q.size q'     `shouldBe` 2
+            M.toList p    `shouldBe` [(2,(0,2))]
+
+    describe "neighbors" $ do
+        it "calculate the distances from closest node to its neighbors" $ do
+            let g = M.fromList [(1,[(2,100),(3,150)]),(2,[(1,100),(3,20)]),(3,[(1,150),(2,20)])]
+            let q = initialize g 2
+
+            let n = neighbors 2 4807 g
+            n `shouldBe` [(1,4907),(3,4827)]
+
+    describe "adjust" $ do
+        it "adjust the distances in the queue if shorter distances are found" $ do
+            let g = M.fromList [(1,[(2,100),(3,150)]),(2,[(1,100),(3,20)]),(3,[(1,150),(2,20)])]
+            let q = initialize g 2
+            let n = [(1,100),(2,4807),(3,20)]
+ 
+            let q' = adjust 2 n q
+            let r = Q.fromList [1 :-> (100,2), 2 :-> (0,2), 3 :-> (20,2)]
+            toList q' `shouldBe` (toList r)
+
+    describe "shortestPath" $ do
+        it "find the shortest path for the neighbors of the closest nodes to source" $ do
+            let g = M.fromList [(1,[(2,100),(3,150)]),(2,[(1,100),(3,20)]),(3,[(1,150),(2,20)])]
+            let q = initialize g 1
+            let p = M.empty
+            let (q',p') = shortestPath g q p
             
-            
+            M.toList p' `shouldBe` [(1,(0,1))]
+            let r = Q.fromList [2 :-> (100,1), 3 :-> (150,1)]
+            toList q' `shouldBe` (toList r)
